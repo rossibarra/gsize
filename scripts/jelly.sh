6243acedd33dbc1315d48667109648d5ce571ae8
rossibarra <rossibarra@ucdavis.edu>
2013-11-28 01:13:42 -0800

    first successful quasi-run, added gitignore

--- scripts/jelly.sh
+++ scripts/jelly.sh
@@ -1,15 +1,17 @@
 #!/bin/zsh -l
 #SBATCH -D /home/jri/projects/genomesize
-#SBATCH -J st
-#SBATCH -o out-%j.txt
+#SBATCH -J jellyfish
+#SBATCH -o outs/out-%j.txt
 #SBATCH -p serial
-#SBATCH -e error-%j.txt
-
+#SBATCH -e errors/error-%j.txt
 
 # script to run jellyfish on paul's files
 
-echo "nicE!"
+NAME="RIMMA0430.1"
+alias JELLY="/home/jri/pkg/bin/jellyfish"
 
-#/home/jri/pkg/bin/jellyfish
+JELLY count -m 20 -o temp/$NAME.counted -t 4 -s 10000000 /group/jrigrp/Share/PaulB_Data/Run1_Rev/$NAME.fastq 
+JELLY merge -o results/$NAME.merged temp/$NAME*
+JELLY histo -o results/$NAME.hist results/$NAME.merged
 
 
