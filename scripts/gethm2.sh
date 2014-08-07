2c71dfd800bb5ea370d4d916cb67a48fb7bc5dd4
rossibarra <rossibarra@ucdavis.edu>
2014-08-06 22:23:42 -0700

    modified gethm2 to write pairs

--- scripts/gethm2.sh
+++ scripts/gethm2.sh
@@ -3,11 +3,13 @@
 #SBATCH -J getFGS
 #SBATCH -o outs/out-%j.txt
 #SBATCH -p bigmem
-#SBATCH --ntasks=4
 #SBATCH -e errors/error-%j.txt
+#SBATCH --array=1-103
 
-for i in $( ls /group/jrigrp3/hapmap2_bam/Disk3CSHL_bams_bwamem/*merged*bam ); do
-out=$( echo $i | sed -e 's/.*\///g' | sed -e 's/bam/fq/g')
-bedtools bamtofastq -i <( samtools view -s 0.02 -u $i ) -fq data/hm2/$out
-done
+module load bwa/0.7.5a
+
+FILE=$( sed -n "$SLURM_ARRAY_TASK_ID"p data/hm2.files | sed -e 's/fq/bam/' )
+
+out=$( echo $FILE | sed -e 's/.*\///g' | sed -e 's/bam/fq/')
+bedtools bamtofastq -i <( samtools view -s 0.05 -u $FILE ) -fq data/hm2/$out.1 -fq2 data/hm2/$out.2
 
