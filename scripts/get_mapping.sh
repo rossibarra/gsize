af0e68d56d33043aa1a5ffa3995ce1618547716b
rossibarra <rossibarra@ucdavis.edu>
2014-08-06 17:53:05 -0700

    fixed get_mapping.sh

--- scripts/get_mapping.sh
+++ scripts/get_mapping.sh
@@ -10,9 +10,9 @@
 module load bwa/0.7.5a
 
 #FILE=$( sed -n "$SLURM_ARRAY_TASK_ID"p data/hm2.files ).bam
-FILE=$( sed -n "$SLURM_ARRAY_TASK_ID"p scripts/RIMMAs_list.txt )
+FILE=$( sed -n "$SLURM_ARRAY_TASK_ID"p data/RIMMAs_list.txt )
 
 echo $FILE
 #gets total reads and mapped reads from bamfile
-samtools view results/$FILE | tee >( cut -f 1 | sort -n | uniq | wc -l >results/$FILE.total ) | cut -f 1,3 | grep -v "*" | cut -f 1 | sort -n | uniq | wc -l > results/$FILE.mapped;
-perl scripts/parsesam.pl <( samtools view results/$FILE | sort -n -k 1 ) results/abundance."$FILE".txt
+samtools view alignments/$FILE | tee >( cut -f 1 | sort -n | uniq | wc -l >results/$FILE.total ) | cut -f 1,3 | grep -v "*" | cut -f 1 | sort -n | uniq | wc -l > results/$FILE.mapped;
+perl scripts/parsesam.pl <( samtools view alignments/$FILE | sort -n -k 1 ) results/abundance."$FILE".txt
